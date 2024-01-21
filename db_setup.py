cde42868cfb943b2a932ac3ca715d7f61454e7ee
root <root@testssh.p4yfaflrnxcuborqyudbu3zfac.bx.internal.cloudapp.net>
2024-01-21 23:45:17 +0000

    git coflict resolutio

--- db_setup.py
+++ db_setup.py
@@ -1,23 +1,30 @@
 import pymysql
 import dbconfig
+
 connection = pymysql.connect(host='localhost',
-user=dbconfig.db_user,
-passwd=dbconfig.db_password)
+                             user=dbconfig.db_user,
+                             passwd=dbconfig.db_password)
+
 try:
-with connection.cursor() as cursor:
-sql = "CREATE DATABASE IF NOT EXISTS crimemap"
-cursor.execute(sql)
-sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
-id int NOT NULL AUTO_INCREMENT,
-latitude FLOAT(10,6),
-longitude FLOAT(10,6),
-date DATETIME,
-category VARCHAR(50),
-description VARCHAR(1000),
-updated_at TIMESTAMP,
-PRIMARY KEY (id)
-)"""
-cursor.execute(sql);
-connection.commit()
+    with connection.cursor() as cursor:
+        # Create database if it doesn't exist
+        sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+        cursor.execute(sql)
+
+        # Create table if it doesn't exist
+        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+                    id int NOT NULL AUTO_INCREMENT,
+                    latitude FLOAT(10,6),
+                    longitude FLOAT(10,6),
+                    date DATETIME,
+                    category VARCHAR(50),
+                    description VARCHAR(1000),
+                    updated_at TIMESTAMP,
+                    PRIMARY KEY (id)
+                 )"""
+        cursor.execute(sql)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
 finally:
-connection.close()
+    connection.close()
--- dbhelper.py
+++ dbhelper.py
@@ -17,18 +17,15 @@ class DBHelper:
                 return cursor.fetchall()
         finally:
             connection.close()
-
-    def add_input(self, data):
-        connection = self.connect()
-        try:
-            # The following introduces a deliberate security flaw.
-            # See section on SQL injection below
-            query = "INSERT INTO crimes (description) VALUES '{}';".format(data)
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-            connection.commit()
-        finally:
-            connection.close()
+   def add_input(self, data):
+       connection = self.connect()
+       try:
+           query = "INSERT INTO crimes (description) VALUES (%s);"
+           with connection.cursor() as cursor:
+               cursor.execute(query, data)
+               connection.commit()
+           finally:
+               connection.close()
 
     def clear_all(self):
         connection = self.connect()
